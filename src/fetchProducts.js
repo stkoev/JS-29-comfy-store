008b561b34f8e0ee311753f43d7a25d4dd00f5e6
Stefan Koev <st.koev@outlook.com>
2022-10-11 13:05:09 +0300

    fetch products

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,12 @@
-import { allProductsUrl } from './utils.js';
+import { allProductsUrl } from "./utils.js";
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(allProductsUrl);
+    return response ? response.json() : response;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 export default fetchProducts;
