96e40251efbd29b155b95289a1174b50eff2be07
Stefan Koev <st.koev@outlook.com>
2022-10-17 13:14:44 +0300

    setup price filter

--- src/pages/products.js
+++ src/pages/products.js
@@ -18,4 +18,5 @@ const loading = getElement(".page-loading");
 display(store, getElement(".products-container"));
 setupSearch(store);
 setupCompanies(store);
+setupPrice(store);
 loading.style.display = "none";
