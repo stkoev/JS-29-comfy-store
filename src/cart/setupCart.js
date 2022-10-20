f805aac9cfcb4ce2386723c3297122c27a111d89
Stefan Koev <st.koev@outlook.com>
2022-10-20 22:58:05 +0300

    addToCartDOM

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,6 +10,29 @@ import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
 
+const cartItemCountDom = getElement(".cart-item-count");
+const cartItemsDom = getElement(".cart-items");
+const cartTotalDom = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
+
 export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    // add item to the cart
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add item to the DOM
+    addToCartDOM(product);
+  } else {
+    // update values
+  }
+
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+init();
